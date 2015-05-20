37ea5e170a51cc2c0ab4f59d22516b864c416589
Hal Blackburn <hwtb2@cam.ac.uk>
2015-05-20 17:20:43 +0100

    More bubbles

--- bubbles.js
+++ bubbles.js
@@ -1,10 +1,24 @@
 import d3 from 'd3';
+import lodash from 'lodash';
+
+var width = 400,
+    height = 600;
+
+var circles = _.times(10, () => {
+    return {
+        cx: Math.random() * width,
+        cy: Math.random() * height,
+        r: 20 + Math.random() * 20
+    }
+});
 
 var chart = d3.select(".chart")
     .attr("width", 400)
     .attr("height", 600)
-  .append("circle")
-    .attr('cx', 60)
-    .attr('cy', 60)
-    .attr('r', 25)
-    .attr('class', 'bubble');
+    .selectAll('circle')
+        .data(circles)
+        .enter().append("circle")
+            .attr('cx', (d) => d.cx)
+            .attr('cy', (d) => d.cy)
+            .attr('r', (d) => d.r)
+            .attr('class', 'bubble');
