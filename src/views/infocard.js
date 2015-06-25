1c6fd56ace235c81fff6b38e9ccbdcd69e0a15f1
Hal Blackburn <hwtb2@cam.ac.uk>
2015-06-25 17:40:50 +0100

    Render titles and abstract in info cards

--- src/views/infocard.js
+++ src/views/infocard.js
@@ -20,9 +20,16 @@ export class InfoCardView extends View {
         this.model = options.model;
     }
 
+    // TODO: define weighted importance values to dmd items to pick useful
+    // subset to show.
+
     render() {
         if(!this.$el.children().length) {
-            this.$el.html(infoCardTemplate());
+            this.$el.html(infoCardTemplate({
+                title: this.getTitle(),
+                subtitles: this.getSubTitles(),
+                abstract: this.getAbstractExcerpt()
+            }));
         }
 
         let svg = $(this.model.svgElement).closest('svg');
@@ -35,6 +42,46 @@ export class InfoCardView extends View {
             // right: width - this.model.position.x - this.model.position.r
             left: this.model.position.x + svgOffset.left
         });
+    }
+
+    getDmdHierachy() {
+        let hit = this.getHit();
+        let dmds = hit.descriptiveMetadata;
+        return _(this.getHit().structurePath)
+            .map(s => dmds[s.descriptiveMetadataID])
+            .value();
+    }
+
+    getHit() {
+        assert(this.model.hit);
+        return this.model.hit;
+    }
+
+    getRootDmd() {
+        return this.getDmdHierachy()[0];
+    }
+
+    getTitle() {
+        let dmd = this.getRootDmd();
+        return dmd.title && dmd.title.displayForm || null;
+    }
+
+    getSubTitles() {
+        return _(this.getDmdHierachy())
+            .slice(1)
+            .map(dmd => dmd.title && dmd.title.displayForm)
+            .filter()
+            .join(' › ')
+    }
+
+    getAbstractExcerpt(dmd) {
+        dmd = dmd || this.getRootDmd();
+
+        if(!dmd.abstract)
+            return null;
+
+        let html = $($.parseHTML(dmd.abstract.displayForm));
+        return html.find('p').addBack('p').first().text() || null;
     }
 }
 _.assign(InfoCardView.prototype, {
