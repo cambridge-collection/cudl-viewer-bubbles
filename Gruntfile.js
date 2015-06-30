e6e6035b46997bbbb0547f658e3da8e57018bfad
Hal Blackburn <hwtb2@cam.ac.uk>
2015-06-30 14:42:47 +0100

    Add production build to Gruntfile and instructions in README

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,6 @@
 var _ = require('lodash');
+var webpack = require('webpack');
+
 var webpackConfig = require('./webpack.config');
 
 module.exports = function(grunt) {
@@ -6,16 +8,33 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+
         webpack: {
             once: webpackConfig,
             watch: _.assign({}, webpackConfig, {
                 watch: true,
                 keepalive: true
-            })
-        }
+            }),
+            production: (function() {
+                var conf = _.clone(webpackConfig);
+
+                var uglify = new webpack.optimize.UglifyJsPlugin({
+                    mangle: true,
+                    compress: true
+                });
+
+                // Enable uglifying
+                conf.plugins = conf.plugins.concat([uglify]);
+
+                return conf;
+            })()
+        },
+
+        clean: ['dist']
     });
 
     grunt.loadNpmTasks('grunt-webpack');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
     // Default task(s).
     grunt.registerTask('default', ['jshint']);
