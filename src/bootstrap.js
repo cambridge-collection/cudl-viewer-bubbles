dcec3fa52d9656def6f2cf12162f33484986e11f
Hal Blackburn <hwtb2@cam.ac.uk>
2015-07-22 16:38:20 +0100

    Fix console.xxx() not working on IE

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,5 +1,7 @@
 // Seems like the polyfill has to be imported before the rest of the code...
 require("babel/polyfill");
+// Ensure we can use console.xxx() on IE without breaking
+require('console-polyfill');
 
 require('../style/similarity.less');
 require('./index');
