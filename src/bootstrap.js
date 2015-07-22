2aefca4393600ec8b5fee0a32728f6d18f58193c
Hal Blackburn <hwtb2@cam.ac.uk>
2015-07-22 17:09:43 +0100

    Correct babel polyfill location

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,5 +1,5 @@
 // Seems like the polyfill has to be imported before the rest of the code...
-require("babel/polyfill");
+require("babel-core/polyfill");
 // Ensure we can use console.xxx() on IE without breaking
 require('console-polyfill');
 
