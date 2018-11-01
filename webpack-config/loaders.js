0e1398eb5295fcd8911f343ee52722b9bccba4f9
James Howe <jmh205@cam.ac.uk>
2018-11-01 15:54:27 +0000

    WIP: Upgrading from Webpack 1 to Webpack 4.

--- webpack-config/loaders.js
+++ webpack-config/loaders.js
@@ -5,30 +5,39 @@ var Config = require('webpack-config').Config;
 
 module.exports = new Config().merge({
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.js$/,
                 include: path.resolve(__dirname, '../src'),
-                loader: 'babel',
-                query: {
-                    cacheDirectory: true,
-                    presets: ['es2015']
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        cacheDirectory: true,
+                    }
                 }
             },
             {
                 test: /\.jade$/,
                 include: path.resolve(__dirname, '../templates'),
-                loader: 'jade'
+                loader: 'pug-loader'
             },
             {
                 test: /\.less$/,
                 include: path.resolve(__dirname, '../style'),
-                loader: 'style!css?sourceMap!postcss?sourceMap!less?sourceMap'
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'postcss-loader',
+                    'less-loader'
+                ]
             },
             {
                 test: /\.png$/,
                 include: path.resolve(__dirname, '../assets'),
-                loader: "url?limit=100000"
+                use: {
+                    loader: 'url-loader',
+                    options: { limit: 100000 }
+                }
             }
         ]
     }
